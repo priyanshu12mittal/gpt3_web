7f25e55c6a6d254cea0898af330e07c77ac3b3cd
priyanshu <priyanshumittal0916@gmail.com>
2023-12-24 20:08:13 +0530

    final comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Footer,Blog,Possibility,Features,WhatGpt3,Header} from './containers';
+import {Cta,Brand,Navbar} from './components';
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar></Navbar>
+        <Header></Header>
+      </div>
+      <Brand></Brand>
+      <WhatGpt3></WhatGpt3>
+      <Features></Features>
+      <Possibility></Possibility>
+      <Cta></Cta>
+      <Blog></Blog>
+      <Footer></Footer>
     </div>
-  );
+  )
 }
 
 export default App;
